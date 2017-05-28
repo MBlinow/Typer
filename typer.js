26917438526eacba13af69ad482b694f0efcd1aa
Oneironaut21 <mblinow@gmail.com>
2017-05-28 16:06:46 -0400

    Logic near complete
    Almost done with the main pieces of logic needed to make this work
    properly.  After that I'll refactor to make the code look good and then
    comes CSS work to make the page look good.

--- typer.js
+++ typer.js
@@ -1,9 +1,11 @@
 
 $(document).ready(function(){
-	timeLimit=500;
-	$('#TopPane').text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+	//Main function.  Starts on page load.
+	timeLimit=5;
+	$('#TopPane').text("The quick brown fox jumps over the lazy dog.");
 	$("#Timer").text('Time: ' +timeLimit);
 	waitToStart();
+	matchCharLengths();
 	
 });
 
@@ -22,4 +24,58 @@ function waitCheck(){
 		$('#length').text('Characters entered:'+fieldlength);
 		inputText=$("#TextBox").val();
 		$('#InputString').text("text: "+inputText);
+		if (fieldlength>0){
+			clearInterval(WaitInterval);
+			startTest();
+		}
+}
+
+function matchCharLengths(){
+	//Sets the input text field to a max length equivalent to the length of the target text.
+	targetLength= $("#TopPane").text().length;
+	$("#TextBox").attr("maxlength", targetLength);
+}
+
+function startTest(){
+	//currently just starts the countdown clock.  Can  be used for any other actions to be taken at start of test.
+	startCountDown();
+}
+
+function startCountDown(){
+	initialTime=timeLimit;
+	countdownInterval= setInterval(clockTimeDown, 1000);
+}
+function clockTimeDown(){
+	//Decrements the countdown clock and check for conditions to end the test
+	//test is ended if the max character length has been met or if the clock has reached 0
+	timeLimit--;
+	$("#Timer").text('Time: ' +timeLimit);
+	if ($("#TextBox").val().length==targetLength){
+		stopTest();
+	}
+	if (timeLimit==0){
+		stopTest();
+	}
+}
+
+function stopTest(){
+	//Called when one of the conditions for the end of the test has been met.
+	//clears countdown for clock and sets the text input field to readonly so no more changes can be made
+	//
+	clearInterval(countdownInterval);
+	$("#TextBox").attr("readonly", "True");
+	gradeTest();
+}
+
+function gradeTest(){
+	input=$("#TextBox").val();
+	expectedInput=$("#TopPane").text();
+	inputWords=input.split(" ");
+	expectedInputWords=expectedInput.split(" ");
+	totalWordsTyped=inputWords.length;
+	calcWordsPerMinute(initialTime-timeLimit, totalWordsTyped)
+}
+function calcWordsPerMinute(elapsedTime, words){
+	var minutes=elapsedTime/60;
+	alert(words/minutes);
 }
