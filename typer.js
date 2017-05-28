a78c1f261889f2e87845af5522d97a2461b3bf81
Oneironaut21 <mblinow@gmail.com>
2017-05-28 12:25:37 -0400

    Progress on basic logic
    I now have a text field that can report when new text is entered into it
    and I can retrieve the string from that field.  Next up is limiting the
    amount of text that can be entered by an amount equal to the target
    text, and getting a countdown timer going.

--- typer.js
+++ typer.js
@@ -1,8 +1,25 @@
 
 $(document).ready(function(){
+	timeLimit=500;
 	$('#TopPane').text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+	$("#Timer").text('Time: ' +timeLimit);
+	waitToStart();
 	
-	$("#BottomPane").keyup(function(){
-            alert($(this).val());
-        });
 });
+
+function waitToStart(){
+	WaitInterval= setInterval(waitCheck, 50)
+	fieldlength=0;
+	//while (fieldlength<50){
+	//	fieldlength= $("#TextBox").val().length;
+	//	$('#length').text('Characters entered:'+fieldlength);
+		//}
+	
+}
+
+function waitCheck(){
+		fieldlength= $("#TextBox").val().length;
+		$('#length').text('Characters entered:'+fieldlength);
+		inputText=$("#TextBox").val();
+		$('#InputString').text("text: "+inputText);
+}
