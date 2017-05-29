7cfbd596575b370f652992fa254ce095e1f921c7
Oneironaut21 <mblinow@gmail.com>
2017-05-29 12:35:16 -0400

    Added word grading and output of graded text
    All essential logic done.  Now I want to clean up the code and then make
    the CSS look nice.  I may also add options for the user to customize the
    target text and initial time.

--- typer.js
+++ typer.js
@@ -1,21 +1,19 @@
 
 $(document).ready(function(){
 	//Main function.  Starts on page load.
-	timeLimit=5;
+	timeLimit=3;
 	$('#TopPane').text("The quick brown fox jumps over the lazy dog.");
-	$("#Timer").text('Time: ' +timeLimit);
+	$("#Timer").text(formatSecondstoMinutes(timeLimit));
 	waitToStart();
 	matchCharLengths();
+	// The quock brown fox jupps over the lazy dog.
+
+
 	
 });
 
 function waitToStart(){
-	WaitInterval= setInterval(waitCheck, 50)
-	fieldlength=0;
-	//while (fieldlength<50){
-	//	fieldlength= $("#TextBox").val().length;
-	//	$('#length').text('Characters entered:'+fieldlength);
-		//}
+	WaitInterval= setInterval(waitCheck, 50);
 	
 }
 
@@ -23,7 +21,7 @@ function waitCheck(){
 		fieldlength= $("#TextBox").val().length;
 		$('#length').text('Characters entered:'+fieldlength);
 		inputText=$("#TextBox").val();
-		$('#InputString').text("text: "+inputText);
+		//$('#InputString').text("text: "+inputText);
 		if (fieldlength>0){
 			clearInterval(WaitInterval);
 			startTest();
@@ -49,13 +47,31 @@ function clockTimeDown(){
 	//Decrements the countdown clock and check for conditions to end the test
 	//test is ended if the max character length has been met or if the clock has reached 0
 	timeLimit--;
-	$("#Timer").text('Time: ' +timeLimit);
+	$("#Timer").text(formatSecondstoMinutes(timeLimit));
 	if ($("#TextBox").val().length==targetLength){
 		stopTest();
 	}
 	if (timeLimit==0){
 		stopTest();
 	}
+}
+function formatSecondstoMinutes(seconds){
+	var minutes=0;
+	
+	if (seconds>60){
+		minutes=seconds/60;
+		minutes=Math.floor(minutes);
+		seconds-=minutes*60;
+	}
+	if (minutes<10){
+		minutes="0"+minutes;
+	}
+	if (seconds<10){
+		seconds="0"+seconds;
+	}
+	var formattedTime="Time "+minutes+":"+seconds;
+
+	return formattedTime;
 }
 
 function stopTest(){
@@ -68,14 +84,45 @@ function stopTest(){
 }
 
 function gradeTest(){
-	input=$("#TextBox").val();
-	expectedInput=$("#TopPane").text();
-	inputWords=input.split(" ");
-	expectedInputWords=expectedInput.split(" ");
-	totalWordsTyped=inputWords.length;
-	calcWordsPerMinute(initialTime-timeLimit, totalWordsTyped)
+	var input=$("#TextBox").val();
+	var expectedInput=$("#TopPane").text();
+	$('#TextBox').remove();
+	var inputWords=input.split(" ");
+	var expectedInputWords=expectedInput.split(" ");
+	var correctWords=0;
+	for (i=0; i<inputWords.length; i++){
+		if (expectedInputWords.length<i){
+			//code for labelling words incorrect for highlighting can go here
+			outputGradedText(inputWords[i], false);
+		}
+		else if (expectedInputWords[i]==inputWords[i]){
+			correctWords++
+			outputGradedText(inputWords[i], true);
+		}
+		else{
+			//code for labelling words incorrect for highlighting can go here
+			outputGradedText(inputWords[i], false);
+		}
+	}
+	calcWordsPerMinute(initialTime-timeLimit, correctWords)
+}
+
+function outputGradedText(word, isCorrect){
+	//var destination=$("#InputString").text();
+	if (isCorrect){
+		$('#BottomPane').append(word.fontcolor('Black')+' ');
+	}
+	else {
+		$('#BottomPane').append(word.fontcolor('Red')+' ');
+
+	}
+	
+}
+function compareWords(input, expected){
+	
 }
 function calcWordsPerMinute(elapsedTime, words){
 	var minutes=elapsedTime/60;
-	alert(words/minutes);
+	//console.log(Math.round(words/minutes)+"Words="+words);
+	$("#results").text("WPM: "+ Math.round(words/minutes))
 }
